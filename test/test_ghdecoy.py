69b4c709f4d40f29bd4bfe800debace394ac822f
tickelton <tickelton@gmail.com>
2015-05-09 20:39:58 +0200

    startet to populate tests

--- test/test_ghdecoy.py
+++ test/test_ghdecoy.py
@@ -1,12 +1,15 @@
 import unittest
 import ghdecoy
 
-
-class TestCalendarFunctions(unittest.TestCase):
+class GHDecoyOnlineTests(unittest.TestCase):
+    """Unit tests form 'ghdecoy.py"""
 
     def test_get_calendar(self):
         cal = ghdecoy.get_calendar('tickelton')
         self.assertEqual(cal[0].find('<svg'), 0)
+
+#class GHDecoyOfflineTests(unittest.TestCase):
+
 
 if __name__ == '__main__':
     unittest.main()
